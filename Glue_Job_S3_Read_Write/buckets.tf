81cb31182e25d396270b58d65cebf7baaae91da1
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2024-01-02 14:12:01 +0530

    Update buckets.tf

--- Glue_Job_S3_Read_Write/buckets.tf
+++ Glue_Job_S3_Read_Write/buckets.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "dc-source-data-bucket" {
 resource "aws_s3_bucket_object" "data-object" {
   bucket = aws_s3_bucket.dc-source-data-bucket.bucket
   key    = "organizations.csv"
-  source = "D:/Terraform_Tutorial/glue_project1/data_file/organizations.csv"
+  source = "<File Path>"
 }
 
 # S3 Bucket for Processed Data
@@ -23,5 +23,5 @@ resource "aws_s3_bucket" "dc-poc-code-bucket" {
 resource "aws_s3_bucket_object" "code-data-object" {
   bucket = aws_s3_bucket.dc-poc-code-bucket.bucket
   key    = "script.py"
-  source = "D:/Terraform_Tutorial/glue_project1/data_file/script.py"
+  source = "<File Path>"
 }
